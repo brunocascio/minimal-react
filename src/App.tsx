6ae725da6b702c0c979e882fce1458d84ecd30df
Bruno Cascio <b.cascio@ptytechnologies.com>
2022-09-13 18:26:03 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import schema from './schema.json';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>{schema.title}</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
